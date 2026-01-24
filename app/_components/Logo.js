250b77099a2ceed53b1f586ed5f12dde1c2931d8
MohitRao <mohitrao2k3@gmail.com>
2026-01-24 12:06:06 +0530

    Make logo stack vertically on mobile screens

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -4,7 +4,10 @@ import logo from "@/public/logo.png";
 
 function Logo() {
   return (
-    <Link href="/" className="flex items-center gap-4 z-10">
+    <Link
+      href="/"
+      className="flex flex-col items-center sm:flex-row sm:items-center gap-2 z-10"
+    >
       <Image
         src={logo}
         quality={100}
@@ -12,7 +15,7 @@ function Logo() {
         width="60"
         alt="The Wild Oasis logo"
       />
-      <span className="text-xl font-semibold text-primary-100">
+      <span className="text-base sm:text-xl font-semibold text-primary-100 text-center sm:text-left">
         The Wild Oasis
       </span>
     </Link>
